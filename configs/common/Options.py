779ea1ff642eb788b74e4db13855a88d4ff4d8e8
Kevin Box <kbox@hmc.edu>
2025-03-19 20:08:36 +0000

    misc: add new options and Se.py for project
    this is for the project runnning

--- configs/common/Options.py
+++ configs/common/Options.py
@@ -197,6 +197,11 @@ def addNoISAOptions(parser):
     parser.add_argument("--l2_assoc", type=int, default=8)
     parser.add_argument("--l3_assoc", type=int, default=16)
     parser.add_argument("--cacheline_size", type=int, default=64)
+    parser.add_argument("--issue_width", type=int, default=8)
+    parser.add_argument(
+        "--numROBEntries", type=int, default=192
+    )  # found 192 in src/cpu/o3/BaseO3CPU.py I think we can just change this param in SE?
+    parser.add_argument("--numWIBEntries", type=int, default=192)
 
     # Enable Ruby
     parser.add_argument("--ruby", action="store_true")
